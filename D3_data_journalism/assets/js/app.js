2d8044b5789657d26adc9722c77631bcabca6fe8
poonam-ux <shaonam@gmail.com>
2021-04-10 06:50:59 -0700

    Update code

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -100,6 +100,58 @@ function makeResponsive() {
         .attr("text-anchor", "middle");
       return textGroup;
     }
+  
+    // Function for updating circles group with new tooltip
+    function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup, textGroup) {
+  
+      if (chosenXAxis === "poverty") {
+        var xLabel = "Poverty (%)";
+      }
+      else if (chosenXAxis === "age") {
+        var xLabel = "Age (Median)";
+      }
+      else {
+        var xLabel = "Household Income (Median)";
+      }
+      if (chosenYAxis === "healthcare") {
+        var yLabel = "Lacks Healthcare (%)";
+      }
+      else if (chosenYAxis === "obesity") {
+        var yLabel = "Obese (%)";
+      }
+      else {
+        var yLabel = "Smokes (%)";
+      }
+  
+      // Initialize tool tip
+      var toolTip = d3.tip()
+        .attr("class", "tooltip d3-tip")
+        .offset([80, -50])
+        .html(function(d) {
+          return (`<strong>${d.state}</strong><br>${xLabel} ${d[chosenXAxis]}<br>${yLabel} ${d[chosenYAxis]}`);
+        });
+      // Create circles tooltip in the chart
+      circlesGroup.call(toolTip);
+      // Create event listeners to display an hdide the circles tooltip
+      circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data, this);
+      })
+        // onmouseout Event
+        .on("mouseout", function(data) {
+          toolTip.hide(data);
+        });
+      // Create text tooltip in the chart
+      textGroup.call(toolTip);
+      // Create Event Listeners to Display and Hide the Text Tooltip
+      textGroup.on("mouseover", function(data) {
+        toolTip.show(data, this);
+      })
+        // onmouseout Event
+        .on("mouseout", function(data) {
+          toolTip.hide(data);
+        });
+      return circlesGroup;
+    }
   }
   
   // When Browser Loads, makeResponsive() is Called
