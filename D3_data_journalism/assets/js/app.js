8fb01677c7d04c8ca776355f58b3f1a25ea838f9
poonam-ux <shaonam@gmail.com>
2021-04-09 20:32:40 -0700

    Read data.csv

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -36,6 +36,22 @@ function makeResponsive() {
     // Append group element & set margins - Shift (Translate) by left and top margins using Transform
     var chartGroup = svg.append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+     // Import Data from the data.csv File
+     d3.csv("assets/data/data.csv")
+     .then(function(censusData) {
+ 
+     // Format/Parse the Data and convert to numerical
+     censusData.forEach(function(data) {
+       data.poverty = +data.poverty;
+       data.age = +data.age;
+       data.income = +data.income;
+       data.healthcare = +data.healthcare;
+       data.obesity = +data.obesity;
+       data.smokes = +data.smokes;
+    });
+    console.log(censusData)
+});
 }
 
 // When the browser loads, makeResponsive() is called.
@@ -43,5 +59,3 @@ makeResponsive();
 
 // When the browser window is resized, makeResponsive() is called.
 d3.select(window).on("resize", makeResponsive);
-
-  
